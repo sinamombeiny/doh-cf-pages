8ee2cc53fd84113269da1097ef51b759c5716032
Sina Mombeiny <SMombeiny+GitHub@proton.me>
2024-05-26 17:29:34 +0330

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const doh = 'https://security.cloudflare-dns.com/dns-query'
-const dohjson = 'https://security.cloudflare-dns.com/dns-query'
+const doh = 'https://freedns.controld.com/no-ads-porn-dating-drugs-gambling-typo-social-malware'
+const dohjson = 'https://freedns.controld.com/no-ads-porn-dating-drugs-gambling-typo-social-malware'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 
